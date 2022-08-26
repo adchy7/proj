2c0f1181ebba5c27aa0c14a6b39087bd443f38f3
adchy7 <adchy7@gmail.com>
2022-08-26 01:33:48 -0400

    v2.0;

--- prog1.py
+++ prog1.py
@@ -16,4 +16,30 @@ print('list1 is:',list1)
 print('First element:',list1[0])
 print(list1[1])
 print(list1[2])
-print(list1[3]) # there is no 4th member
+# print(list1[3]) # there is no 4th member
+
+print('###############')
+item = list1[0]
+print(item)
+
+item = list1[1]
+print(item)
+
+item = list1[2]
+print(item)
+
+#  item = list1[5]  # IndexError: list index out of range
+#  print(item)
+
+print('###############')
+item = list1[-1]  # show last item
+print(item)
+
+item = list1[-2]  # show 2nd last item
+print(item)
+
+item = list1[-3]  # show 3rd last item
+print(item)
+
+
+
