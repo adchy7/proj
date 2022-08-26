d5fe87ff86694883a77855ed4d0bedabc930fd04
adchy7 <adchy7@gmail.com>
2022-08-26 01:17:38 -0400

    v1.0;

--- prog1.py
+++ prog1.py
@@ -1,8 +1,19 @@
-# This is my first Python program.
-# This will print 'Hello, World!' as the output
 
-print ("Hello, World!");
+#lists are ordered, mutable, allows duplicate elements
 
-list1 = ['one', 'two', 'three']
-
+list1 = ['banana', 'cherry', 'apple']
 print(list1)
+
+list2  = list()
+print(list2)       # will print empty list
+
+list3 = [5, True, "apple","apple"]
+print(list3)
+
+print('\n')
+print('###############')
+print('list1 is:',list1)
+print('First element:',list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3]) # there is no 4th member
